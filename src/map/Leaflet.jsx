4d7a64767cf5f97ae58db01d8a5d4c06152f4936
Andrew Cook <andrewjcook93@gmail.com>
2025-04-20 22:00:59 -0500

    add theaters and music venues

--- src/map/Leaflet.jsx
+++ src/map/Leaflet.jsx
@@ -14,15 +14,12 @@ const Leaflet = () => {
       // open street map
       var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
-          attribution: '© OpenStreetMap'
+          // attribution: '© OpenStreetMap'
       });
       
       // var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       //     maxZoom: 19,
       //     attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
-      
-
-
 
       var map = L.map('map', {
           center: [29.7604, -95.3698],
@@ -31,7 +28,7 @@ const Leaflet = () => {
       });
 
       var baseMaps = {
-          "OpenStreetMap": osm,
+          // "OpenStreetMap": osm,
           // "OpenStreetMap.HOT": osmHOT
       };
       
@@ -46,83 +43,86 @@ const Leaflet = () => {
           // "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
       };
 
-      // var crownHill = L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.')
-      // var rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
-          
-      // var parks = L.layerGroup([crownHill, rubyHill]);
-      // var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      //     maxZoom: 19,
-      //     attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
-      // });
-
-      // // layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
-      // layerControl.addOverlay(parks, "Parks");
-
-
-
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
 
-      const customIcon = L.icon({
-        iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // example icon (map pin)
-        iconSize: [38, 38], // size of the icon
-        iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
-        popupAnchor: [0, -38] // point from which the popup should open relative to the iconAnchor
-      });
+      // const customIcon = L.icon({
+      //   iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // example icon (map pin)
+      //   iconSize: [38, 38], // size of the icon
+      //   iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+      //   popupAnchor: [0, -38] // point from which the popup should open relative to the iconAnchor
+      // });
 
-      // Loop through the locations array and create markers
 
+      // Layers 1 of 3: Create Layer
       const restaurants = L.layerGroup()
+      const bars = L.layerGroup()
+      const movies = L.layerGroup()
+      const music = L.layerGroup()
 
+      // Start locations loop
       locations.forEach(location => {
 
-      // Build Map Icons
-      let mapIconUrl = ''
-      if (location.category == "food") {
-        mapIconUrl = 'https://i.imgur.com/s2BRLZD.png'
-      }
+        // Layers 2 of 4: Assign Icon Image
+        let mapIconUrl = ''
+        if (location.category == "food") {
+          mapIconUrl = 'https://i.imgur.com/s2BRLZD.png';
+        } else if (location.category == "bar") {
+          mapIconUrl = 'https://i.imgur.com/VtHXOyL.png';
+        } else if (location.category == "movies") {
+          mapIconUrl = 'https://i.imgur.com/DgxjJXt.png';
+        } else if (location.category == 'music') {
+          mapIconUrl = 'https://i.imgur.com/2SrhTIj.png';
+        }
 
-      const customIcon = L.icon({
-        iconUrl: mapIconUrl,
-        iconSize: [38, 38],  // size of the icon
-        iconAnchor: [19, 38], // anchor point of the icon (center of the bottom)
-        popupAnchor: [0, -38] // popup should appear above the icon
+        const customIcon = L.icon({
+          iconUrl: mapIconUrl,
+          iconSize: [38, 38],  // size of the icon
+          iconAnchor: [19, 38], // anchor point of the icon (center of the bottom)
+          popupAnchor: [0, -38] // popup should appear above the icon
+        });
+    
+        // Create the marker for the location
+        const marker = L.marker([location.coordinates[0], location.coordinates[1]], { icon: customIcon }).addTo(map);
+    
+        // Layers 3 of 4: Assign Marker to Layer
+        if (location.category == "food") {
+          // console.log(location.name, "is a restaurant")
+          restaurants.addLayer(marker);
+        } else if (location.category == "bar") {
+          bars.addLayer(marker);
+        } else if (location.category == "movies") {
+          movies.addLayer(marker);
+        } else if (location.category == "music") {
+          music.addLayer(marker);
+        }
+
+        // console.log(Array.isArray(Object.values(location.description)))
+        // const descriptionArray = Object.values(location.description)
+        // console.log(descriptionArray)
+
+        // Bind a popup to the marker
+        marker.bindPopup(`
+          <img src=${location.img} width=200 /><br/>
+          <b>${location.name}</b><br>
+          <a href=${location.website} target='_blank'>
+              Vist Website</a>
+          <ul>
+              <li>${location.description[0]}</li>
+              <li>${location.description[1]}</li>
+              <li>${location.description[2]}</li>
+          </ul>`);
       });
-  
-      // Create the marker for the location
-      const marker = L.marker([location.coordinates[0], location.coordinates[1]], { icon: customIcon }).addTo(map);
-  
-      // add to layer
-      if (location.category == "food") {
-        // console.log(location.name, "is a restaurant")
-        restaurants.addLayer(marker)
-      }
+      // End locations loop
 
-      console.log(Array.isArray(Object.values(location.description)))
-      const descriptionArray = Object.values(location.description)
-      console.log(descriptionArray)
-
-      // Bind a popup to the marker
-      marker.bindPopup(`
-        <img src=${location.img} width=200 /><br/>
-        <b>${location.name}</b><br>
-        <a href=${location.website} target='_blank'>
-            Vist Website</a>
-        <ul>
-            <li>${location.description[0]}</li>
-            <li>${location.description[1]}</li>
-            <li>${location.description[2]}</li>
-        </ul>`);
-
-      });
-
+      // Layers 4 of 4: Add Controls for Each Layer
       layerControl.addOverlay(restaurants, "Restaurants");
-
-      console.log(locations.length)
-
+      layerControl.addOverlay(bars, "Bars");
+      layerControl.addOverlay(movies, "Movie Theaters");
+      layerControl.addOverlay(music, "Music Venues");
     }
   
   }, []); 
--- src/map/locations.js
+++ src/map/locations.js
@@ -11,7 +11,7 @@ const locations = [
     {
         name: 'Taste of Korea',
         category: 'food',
-        coordinates: [30.02963651436055, -95.20326430114102],
+        coordinates: [29.997460350283827, -95.19376149496405],
         img: 'https://i.imgur.com/vixRkWH.png',
         website: 'https://www.google.com/search?q=taste+of+korea+houston+restaurant&client=opera-gx&hs=1xJ&sca_esv=4aa7399608798402&sxsrf=AHTn8zptgKriNcwjnfSJFGJA5pfFsredLw%3A1745096166437&ei=5g0EaPy7Gta4wN4PuMiVsAc&ved=0ahUKEwj8xuXo_eSMAxVWHNAFHThkBXYQ4dUDCBI&uact=5&oq=taste+of+korea+houston+restaurant&gs_lp=Egxnd3Mtd2l6LXNlcnAiIXRhc3RlIG9mIGtvcmVhIGhvdXN0b24gcmVzdGF1cmFudDIFECEYoAEyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKABMgUQIRirAkiBLFCrBViYK3AIeAGQAQCYAacBoAH6DKoBBDE0LjO4AQPIAQD4AQGYAhmgAuENwgIKEAAYsAMY1gQYR8ICDRAAGLADGNYEGEcYyQPCAg4QABiABBiwAxiSAxiKBcICChAjGIAEGCcYigXCAgsQLhiABBjHARivAcICBhAAGBYYHsICAhAmwgILEAAYgAQYhgMYigXCAggQABiABBiiBMICBRAAGO8FwgIaEC4YgAQYxwEYrwEYlwUY3AQY3gQY4ATYAQHCAgUQABiABMICEBAuGIAEGBQYhwIYxwEYrwHCAhAQLhiABBhDGMcBGIoFGK8BwgIIEAAYgAQYyQPCAgUQIRifBZgDAIgGAZAGCboGBggBEAEYFJIHBDIxLjSgB9iXAbIHBDEzLjS4B8UN&sclient=gws-wiz-serp',
         description: ['friendly owner', 'hot stone bowl', 'unreliable hours']
@@ -43,7 +43,7 @@ const locations = [
     {
         name: 'The Taco Stand',
         category: 'food',
-        coordinates: [29.808536107018863, -95.3899774703192],
+        coordinates: [29.80467537187449, -95.40940911953685],
         img: 'https://i.imgur.com/iCIE0bY.png',
         website: 'https://tacostandhtx.com/',
         description: ['breakfast tacos', 'brisket & egg', 'chorizo & egg']
@@ -51,7 +51,7 @@ const locations = [
     {
         name: 'Insomnia Cookies',
         category: 'food',
-        coordinates: [29.720482900878075, -95.40498712408545],
+        coordinates: [29.717783381639077, -95.41698820235376],
         img: 'https://i.imgur.com/0cyEvM4.png',
         website: 'https://insomniacookies.com/',
         description: ['open late', 'deluxe cookies', 'ice cream']         
@@ -59,7 +59,7 @@ const locations = [
     {
         name: 'The Rice Box - Heights',
         category: 'food',
-        coordinates: [29.808791599839974, -95.3801500022352],
+        coordinates: [29.803866550751028, -95.40101375817602],
         img: 'https://i.imgur.com/fBLTpv2.png',
         website: 'https://i.imgur.com/fBLTpv2.png',
         description: ['open late', 'kung pao beef', 'chow fun']
@@ -67,7 +67,7 @@ const locations = [
     {
         name: 'The Rice Box - Rice Village',
         category: 'food',
-        coordinates: [29.716584073640465, -95.41454967209602],
+        coordinates: [29.71659805044453, -95.41461404468372],
         img: 'https://i.imgur.com/kac6XNX.png',
         website: 'https://i.imgur.com/fBLTpv2.png',
         description: ['open late', 'kung pao beef', 'chow fun']
@@ -75,18 +75,120 @@ const locations = [
     {
         name: "The Monk's - Humble",
         category: 'food',
-        coordinates: [30.01789841752994, -95.22461673848046],
+        coordinates: [30.003620305655076, -95.27773996608327],
         img: 'https://i.imgur.com/X6qqegt.png',
         website: 'https://www.monkshumble.com/',
         description: ['indian', 'very spicy', 'samosas']
-    }
+    },
+    {
+        name: 'Betelgeuse Betelgeuse - Washington',
+        category: 'food',
+        coordinates: [29.768873079874567, -95.37747622659104],
+        img: 'https://i.imgur.com/3EB93eb.png',
+        website: 'https://www.betelgeusehou.com/',
+        description: ['pizza', 'full bar', 'outdoor seating']
+    },
+    {
+        name: 'Betelgeuse Betelgeuse - Montrose',
+        category: 'food',
+        coordinates: [29.73385004922882, -95.3868317708331],
+        img: 'https://i.imgur.com/3EB93eb.png',
+        website: 'https://www.betelgeusehou.com/',
+        description: ['pizza', 'full bar', 'outdoor seating']
+    },
+    {
+        name: "Nielson's Delicatessen",
+        category: 'food',
+        coordinates: [29.733001745772487, -95.45125833534226],
+        img: 'https://i.imgur.com/2j3MCnc.png',
+        website: 'https://www.nielsensdelicatessen.com/',
+        description: ['historic', 'fresh sandwiches', 'quality deli meat']
+    },
     // Bars
+    {
+        name: 'Ador',
+        category: 'bar',
+        coordinates: [29.753411079893684, -95.38650062691997],
+        img: 'https://i.imgur.com/JTsWFwL.png',
+        website: 'https://adorhouston.com/',
+        description: ['happy hour', 'hookah', 'daiquiris']
+    },
+    {
+        name: 'Locker Room',
+        category: 'bar',
+        coordinates: [29.72193649943075, -95.36747176543562],
+        img: 'https://i.imgur.com/1MXxNyt.png',
+        website: 'https://www.google.com/search?q=locker+room+houston+bar&oq=locker+room+houston+bar&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MhYIARAuGK8BGMcBGJECGIAEGIoFGI4FMg0IAhAAGJECGIAEGIoFMggIAxAAGBYYHjIICAQQABgWGB4yCAgFEAAYFhgeMggIBhAAGBYYHjIICAcQABgWGB4yCAgIEAAYFhge0gEIMzQzMWoxajSoAgCwAgE&sourceid=chrome&ie=UTF-8',
+        description: ['happy hour', 'hookah', 'shots']
+    },
+    {
+        name: 'CRU - Downtown',
+        category: 'bar',
+        coordinates: [29.762292281329156, -95.3616406517955],
+        img: 'https://i.imgur.com/crlJaYk.png',
+        website: 'https://crulounge.com/',
+        description: ['neon lit', 'hookah', 'dj']
+    },
+    {
+        name: 'CRU - Galleria',
+        category: 'bar',
+        coordinates: [29.7162290256621, -95.49744793118549],
+        img: 'https://i.imgur.com/TlPjazB.png',
+        website: 'https://crulounge.com/',
+        description: ['neon lit', 'hookah', 'dj']
+    },
+    {
+        name: "Warren's Inn",
+        category: 'bar',
+        coordinates: [29.76239315781994, -95.36150769972693],
+        img: 'https://i.imgur.com/6eqneAi.png',
+        website: 'https://www.google.com/search?q=warrens+houston&oq=warrens+houston&gs_lcrp=EgZjaHJvbWUqCggAEAAY4wIYgAQyCggAEAAY4wIYgAQyEAgBEC4YrwEYxwEYgAQYjgUyCAgCEAAYFhgeMggIAxAAGBYYHjIICAQQABgWGB4yCAgFEAAYFhgeMggIBhAAGBYYHjIICAcQABgWGB4yCAgIEAAYFhgeMggICRAAGBYYHtIBCDQzNDJqMWo0qAIAsAIB&sourceid=chrome&ie=UTF-8',
+        description: ['historic bar', 'friendly staff', 'tavern feel']
+    },
+    // Movie Theaters
+    {
+        name: 'AMC Willowbrook 24',
+        category: 'movies',
+        coordinates: [29.95353569926177, -95.54006845262985],
+        img: 'https://i.imgur.com/7j18fGs.png',
+        website: 'https://www.amctheatres.com/movie-theatres/houston/amc-willowbrook-24',
+        description: ['premium seats', 'digital IMAX', 'Dolby Digital']
+    },
+    {
+        name: 'AMC Deerbrook 24',
+        category: 'movies',
+        coordinates: [30.010016013578735, -95.27108698884724],
+        img: 'https://i.imgur.com/hkIrPr8.png',
+        website: 'https://www.amctheatres.com/movie-theatres/houston/amc-deerbrook-24',
+        description: ['premium seats', 'digital IMAX', 'Dolby Digital']
+    },
+    {
+        name: 'AMC Houston 8',
+        category: 'movies',
+        coordinates: [29.761902952575078, -95.36625280604103],
+        img: 'https://i.imgur.com/Ft6rlvE.png',
+        website: 'https://www.amctheatres.com/movie-theatres/houston/amc-houston-8',
+        description: ['premium seats', 'digital IMAX', 'Dolby Digital']
+    },
+    // Music Venues
+    {
+        name: 'White Oak Music Hall',
+        category: 'music',
+        coordinates: [29.786245855756498, -95.36701642980238],
+        img: 'https://i.imgur.com/lh9c1Eu.png',
+        website: 'https://whiteoakmusichall.com/',
+        description: ['upstairs room', 'downstairs room', 'lawn shows']
+    },
+    {
+        name: 'House of Blues - Houston',
+        category: 'music',
+        coordinates: [29.754012620241973, -95.36330400949777],
+        img: 'https://i.imgur.com/k5dSSlt.png',
+        website: 'https://www.livenation.com/venue/KovZpZAE6k1A/house-of-blues-houston-events?awtrc=true&c=SEM_LNVenuesAutomation_ggl_18519938348_142072537436_house%20of%20blues&GCID=0&gad_source=1&gbraid=0AAAAAC1KtUphGOVv8Rofb-wxM6BpSibhT&gclid=Cj0KCQjwtpLABhC7ARIsALBOCVoSlXtVubE-SSFspbLCjloWham5bis77jY47pOXUM-yH7Rv6b9LRIEaAhEiEALw_wcB&gclsrc=aw.ds',
+        description: ['house of blues', 'bronze peacock', 'foundation room']
+    }
 
     // Parks
-
-    // Music Venue
-
-    // Theater
 
     // Photo Opp
 
