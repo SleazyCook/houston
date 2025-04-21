a257f8fc358e51c611647689d286ee58719572a5
Andrew Cook <andrewjcook93@gmail.com>
2025-04-21 09:39:38 -0500

    add 10+ restaurants

--- src/map/locations.js
+++ src/map/locations.js
@@ -1,8 +1,19 @@
+// Restaurants
+// Bars
+// Movie Theaters
+// Music Venues
+// Parks
+// Photo Opp
+// Unique (nrg, toyota, kemah)
+// Bookstores
+// Markets
+
 const locations = [
     // Restaurants
     {
         name: 'Wannabao',
         category: 'food',
+        subcategory: 'dumplings',
         coordinates: [29.747720290049234, -95.38028603278978],
         img: 'https://i.imgur.com/yzjy5Ek.png',
         website: 'https://wannabao.com/',
@@ -11,6 +22,7 @@ const locations = [
     {
         name: 'Taste of Korea',
         category: 'food',
+        subcategory: 'ricebowl',
         coordinates: [29.997460350283827, -95.19376149496405],
         img: 'https://i.imgur.com/vixRkWH.png',
         website: 'https://www.google.com/search?q=taste+of+korea+houston+restaurant&client=opera-gx&hs=1xJ&sca_esv=4aa7399608798402&sxsrf=AHTn8zptgKriNcwjnfSJFGJA5pfFsredLw%3A1745096166437&ei=5g0EaPy7Gta4wN4PuMiVsAc&ved=0ahUKEwj8xuXo_eSMAxVWHNAFHThkBXYQ4dUDCBI&uact=5&oq=taste+of+korea+houston+restaurant&gs_lp=Egxnd3Mtd2l6LXNlcnAiIXRhc3RlIG9mIGtvcmVhIGhvdXN0b24gcmVzdGF1cmFudDIFECEYoAEyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKABMgUQIRirAkiBLFCrBViYK3AIeAGQAQCYAacBoAH6DKoBBDE0LjO4AQPIAQD4AQGYAhmgAuENwgIKEAAYsAMY1gQYR8ICDRAAGLADGNYEGEcYyQPCAg4QABiABBiwAxiSAxiKBcICChAjGIAEGCcYigXCAgsQLhiABBjHARivAcICBhAAGBYYHsICAhAmwgILEAAYgAQYhgMYigXCAggQABiABBiiBMICBRAAGO8FwgIaEC4YgAQYxwEYrwEYlwUY3AQY3gQY4ATYAQHCAgUQABiABMICEBAuGIAEGBQYhwIYxwEYrwHCAhAQLhiABBhDGMcBGIoFGK8BwgIIEAAYgAQYyQPCAgUQIRifBZgDAIgGAZAGCboGBggBEAEYFJIHBDIxLjSgB9iXAbIHBDEzLjS4B8UN&sclient=gws-wiz-serp',
@@ -19,6 +31,7 @@ const locations = [
     {
         name: 'Xiaolong Dumpling',
         category: 'food',
+        subcategory: 'dumplings',
         coordinates: [29.751968194944496, -95.385205425497],
         img: 'https://i.imgur.com/mL0K4FI.png',
         website: 'https://pos.chowbus.com/online-ordering/store/Xiaolong-Dumpling/14867',
@@ -27,6 +40,7 @@ const locations = [
     {
         name: 'Mensho',
         category: 'food',
+        subcategory: 'ramen',
         coordinates: [29.702776279471518, -95.55277518132357],
         img: 'https://i.imgur.com/vi8bS5Z.png',
         website: 'https://mensho.com/',
@@ -35,6 +49,7 @@ const locations = [
     {
         name: 'Blue Sushi',
         category: 'food',
+        subcategory: 'sushi',
         coordinates: [29.782854094917795, -95.40735213529982],
         img: 'https://i.imgur.com/TI5ybqr.png',
         website: 'https://www.bluesushisakegrill.com/locations/m-k-t-heights',
@@ -43,6 +58,7 @@ const locations = [
     {
         name: 'The Taco Stand',
         category: 'food',
+        subcategory: 'tacos',
         coordinates: [29.80467537187449, -95.40940911953685],
         img: 'https://i.imgur.com/iCIE0bY.png',
         website: 'https://tacostandhtx.com/',
@@ -51,6 +67,7 @@ const locations = [
     {
         name: 'Insomnia Cookies',
         category: 'food',
+        subcategory: 'dessert',
         coordinates: [29.717783381639077, -95.41698820235376],
         img: 'https://i.imgur.com/0cyEvM4.png',
         website: 'https://insomniacookies.com/',
@@ -59,6 +76,7 @@ const locations = [
     {
         name: 'The Rice Box - Heights',
         category: 'food',
+        subcategory: 'ricebowl',
         coordinates: [29.803866550751028, -95.40101375817602],
         img: 'https://i.imgur.com/fBLTpv2.png',
         website: 'https://i.imgur.com/fBLTpv2.png',
@@ -67,6 +85,7 @@ const locations = [
     {
         name: 'The Rice Box - Rice Village',
         category: 'food',
+        subcategory: 'ricebowl',
         coordinates: [29.71659805044453, -95.41461404468372],
         img: 'https://i.imgur.com/kac6XNX.png',
         website: 'https://i.imgur.com/fBLTpv2.png',
@@ -75,6 +94,7 @@ const locations = [
     {
         name: "The Monk's - Humble",
         category: 'food',
+        subcategory: 'ricebowl',
         coordinates: [30.003620305655076, -95.27773996608327],
         img: 'https://i.imgur.com/X6qqegt.png',
         website: 'https://www.monkshumble.com/',
@@ -83,6 +103,7 @@ const locations = [
     {
         name: 'Betelgeuse Betelgeuse - Washington',
         category: 'food',
+        subcategory: 'pizza',
         coordinates: [29.768873079874567, -95.37747622659104],
         img: 'https://i.imgur.com/3EB93eb.png',
         website: 'https://www.betelgeusehou.com/',
@@ -91,6 +112,7 @@ const locations = [
     {
         name: 'Betelgeuse Betelgeuse - Montrose',
         category: 'food',
+        subcategory: 'pizza',
         coordinates: [29.73385004922882, -95.3868317708331],
         img: 'https://i.imgur.com/3EB93eb.png',
         website: 'https://www.betelgeusehou.com/',
@@ -99,11 +121,146 @@ const locations = [
     {
         name: "Nielson's Delicatessen",
         category: 'food',
+        subcategory: 'sandwiches',
         coordinates: [29.733001745772487, -95.45125833534226],
         img: 'https://i.imgur.com/2j3MCnc.png',
         website: 'https://www.nielsensdelicatessen.com/',
         description: ['historic', 'fresh sandwiches', 'quality deli meat']
     },
+    {
+        name: "Portillo's",
+        category: 'food',
+        subcategory: 'hotdogs',
+        coordinates: [29.964633753145392, -95.54078033344278],
+        img: '',
+        website: '',
+        description: ['Chicago Favorite', 'Original Beef', 'Chicago Hotdog']
+    },
+    {
+        name: 'In & Out - Willowbrook',
+        category: 'food',
+        subcategory: 'burgers',
+        coordinates: [29.963688061809677, -95.5421339703664],
+        img: '',
+        website: '',
+        description: ['California Favorite', 'Flying Dutchman w/ Onions', 'Flying Ducthman w/ Tomatoes']
+    },
+    {
+        name: 'Cheba Hut - Lower Heights',
+        category: 'food',
+        subcategory: 'sanwiches',
+        coordinates: [29.77234244004, -95.37822662667772],
+        img: '',
+        website: '',
+        description: ["'Toasted' Subs", 'Open Late', 'Stoner Themed']
+    },
+    {
+        name: 'Blodgett Hall',
+        category: 'food',
+        coordinates: [29.722847269152375, -95.37002136428438],
+        img: '',
+        website: '',
+        description: ['Food Hall', 'Open Late', 'BYOB']
+    },
+    {
+        name: 'Homeslice',
+        category: 'food',
+        subcategory: 'pizza',
+        coordinates: [29.738887728012283, -95.37986181031256],
+        img: '',
+        website: '',
+        description: ['Austin Favorite', 'Pizza by the Slice', 'Italian Subs']
+    },
+    {
+        name: 'Breakfast Klub',
+        category: 'food',
+        subcategory: 'chicken',
+        coordinates: [29.73853074643878, -95.38059909339351],
+        img: '',
+        website: '',
+        description: ['Open Early', 'Expect a wait', 'Chicken and Waffles']
+    },
+    {
+        name: 'Iekei',
+        category: 'food',
+        subcategory: 'sushi',
+        coordinates: [29.935672247210096, -95.25000159496145],
+        img: '',
+        website: '',
+        description: []
+    },    
+    {
+        name: "Frank's Pizza",
+        category: 'food',
+        subcategory: 'pizza',
+        coordinates: [29.761548417674607, -95.36217053914736],
+        img: '',
+        website: '',
+        description: ['Historic', 'Pizza by the Slice', 'Open Late']
+    },
+    {
+        name: "Tacos Aracely's",
+        category: 'food',
+        subcategory: 'tacos',
+        coordinates: [29.840820719855106, -95.35421491888364],
+        img: '',
+        website: '',
+        description: ['Delicious Tacos', 'Open Early', 'Mexican Potato w/ Sausage']
+    },
+    {
+        name: "Frenchy's",
+        category: 'food',
+        subcategory: 'chicken',
+        coordinates: [29.72911471639174, -95.33228021691421],
+        img: '',
+        website: '',
+        description: ['Houston Favorite', 'Best Fried Chicken', 'Jalapenos']
+    },
+    {
+        name: 'La Guadalupana',
+        category: 'food',
+        subcategory: 'tacos',
+        coordinates: [29.75397083325556, -95.38335480760226],
+        img: '',
+        website: '',
+        description: ['Breakfast tacos', '', '']
+    },
+    {
+        name: 'Velvet Taco - Heights',
+        category: 'food',
+        subcategory: 'tacos',
+        coordinates: [29.804085974827032, -95.41029345468208],
+        img: '',
+        website: '',
+        description: []
+    },
+    {
+        name: 'Velvet Taco - Rice Village',
+        category: 'food',
+        subcategory: 'tacos',
+        coordinates: [29.77027627032481, -95.41004657788817],
+        img: '',
+        website: '',
+        description: []
+    },
+    {
+        name: 'Velvet Taco - Montrose',
+        category: 'food',
+        subcategory: 'tacos',
+        coordinates: [29.744484475293824, -95.39052608843915],
+        img: '',
+        website: '',
+        description: []
+    },
+    // {
+    //     name: '',
+    //     category: 'food',
+    //     subcategory: 'tacos',
+    //     coordinates: [],
+    //     img: '',
+    //     website: '',
+    //     description: []
+    // },
     // Bars
     {
         name: 'Ador',
@@ -186,13 +343,25 @@ const locations = [
         img: 'https://i.imgur.com/k5dSSlt.png',
         website: 'https://www.livenation.com/venue/KovZpZAE6k1A/house-of-blues-houston-events?awtrc=true&c=SEM_LNVenuesAutomation_ggl_18519938348_142072537436_house%20of%20blues&GCID=0&gad_source=1&gbraid=0AAAAAC1KtUphGOVv8Rofb-wxM6BpSibhT&gclid=Cj0KCQjwtpLABhC7ARIsALBOCVoSlXtVubE-SSFspbLCjloWham5bis77jY47pOXUM-yH7Rv6b9LRIEaAhEiEALw_wcB&gclsrc=aw.ds',
         description: ['house of blues', 'bronze peacock', 'foundation room']
-    }
+    },
+    {
+        name: 'Warehouse Live',
+        category: 'music',
+        coordinates: [29.746012478013213, -95.3750447679838],
+        img: '',
+        website: '',
+        description: []
+    },
 
     // Parks
 
     // Photo Opp
 
     // Unique (nrg, toyota)
+
+    // bookstores
+
+    // markets
 ]
 
 export default locations;
