bfaf235e3c3ee24dc1131384f91a039c60b7da4a
Andrew Cook <andrewjcook93@gmail.com>
2025-04-29 23:05:07 -0500

    add scrollToTop for nav buttons

--- src/app/Navigation.jsx
+++ src/app/Navigation.jsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
 
 const Navigation = ({ setLat, setLon, setZoom }) => {
-  // reset Map Coordinates to Houston center
+  // Reset Map Coordinates to Houston center
   const resetCoordinates = () => {
     setLat(29.7604);
     setLon(-95.3698);
     setZoom(11);
   };
-    return (
-      <div className='navigation'>
-      <Link to="/" onClick={resetCoordinates}>Map</Link>
-        <Link to='/food'>Restaurants</Link>
-        <Link to='/activities'>Activities</Link>
-        <Link to='/about'>About</Link>
-      </div>
-    );
+
+  // Scroll to top of the page
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
-  
-  export default Navigation;
+
+  // Combines scroll + optional coordinate reset
+  const handleClick = (shouldResetCoordinates = false) => () => {
+    scrollToTop();
+    if (shouldResetCoordinates) resetCoordinates();
+  };
+
+  return (
+    <div className="navigation">
+      <Link to="/" onClick={handleClick(true)}>Map</Link>
+      <Link to="/food" onClick={handleClick()}>Restaurants</Link>
+      <Link to="/activities" onClick={handleClick()}>Activities</Link>
+      <Link to="/about" onClick={handleClick()}>About</Link>
+    </div>
+  );
+};
+
+export default Navigation;
