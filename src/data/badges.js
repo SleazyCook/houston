8c3ff6601ff5a3a0f6b09a3d03bd037b1fc7232e
Andrew Cook <andrewjcook93@gmail.com>
2025-05-11 13:15:30 -0500

    add hookah badge

--- src/data/badges.js
+++ src/data/badges.js
@@ -1,7 +1,8 @@
 const badges = {
     openLate: 'https://i.imgur.com/466PcN4.png',
     contactless: 'https://i.imgur.com/EM4ZPgf.png',
-    driveThru: 'https://i.imgur.com/XHMMJ93.png'
+    driveThru: 'https://i.imgur.com/XHMMJ93.png',
+    hookah: 'https://i.imgur.com/fDeryi7.png'
 }
 
 export default badges;
