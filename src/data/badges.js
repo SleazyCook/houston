2fb95d49387daaef7b08a0647f9ca2c263a1c582
Andrew Cook <andrewjcook93@gmail.com>
2025-05-11 15:24:33 -0500

    add cash only badge

--- src/data/badges.js
+++ src/data/badges.js
@@ -2,7 +2,8 @@ const badges = {
     openLate: 'https://i.imgur.com/466PcN4.png',
     contactless: 'https://i.imgur.com/EM4ZPgf.png',
     driveThru: 'https://i.imgur.com/XHMMJ93.png',
-    hookah: 'https://i.imgur.com/fDeryi7.png'
+    hookah: 'https://i.imgur.com/fDeryi7.png',
+    cashOnly: 'https://i.imgur.com/ppDeNiZ.png'
 }
 
 export default badges;
