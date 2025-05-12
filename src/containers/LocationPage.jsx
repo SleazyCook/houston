d779eb20e81c390d79c149311558141f014c3009
Andrew Cook <andrewjcook93@gmail.com>
2025-05-12 13:24:47 -0500

    add basic styles and functionality to dynamic location route pages

--- src/containers/LocationPage.jsx
+++ src/containers/LocationPage.jsx
@@ -1,5 +1,5 @@
 // pages/LocationPage.js
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import Location from '../components/Location.jsx';
 
@@ -11,6 +11,22 @@ import generateLocationSlug from '../utils/slug.js';
 export default function LocationPage() {
   const { slug } = useParams();
   const location = locations.find(loc => generateLocationSlug(loc) === slug);
+
+  const renderLink = () => {
+    if (location.subcategory && location.category !== 'attraction') {
+      return (
+        <Link to="/food" className="see-more-link">
+          See more restaurants
+        </Link>
+      );
+    } else {
+      return (
+        <Link to="/activities" className="see-more-link">
+          See more activities
+        </Link>
+      );
+    }
+  };
 
   if (!location) 
     return(
@@ -26,8 +42,40 @@ export default function LocationPage() {
     );
 
   return (
-    <div className='location-page'>
-        <Location item={location}/>
+    <div
+        className="location-page"
+        style={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: '150px',
+        paddingBottom: '150px',
+        }}
+    >
+        <div
+        className="background-overlay"
+        style={{
+          position: 'absolute', // Position overlay over content
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url(${location.img})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 0.7, // Set the opacity of the background image here
+          zIndex: -1, // Make sure the overlay stays behind the content
+        }}></div>
+
+        <div>
+            <Location item={location}/>
+            <div className="see-more-container">
+                {renderLink()}
+            </div>
+        </div>
+
+        
       {/* Render other location data */}
     </div>
   );
