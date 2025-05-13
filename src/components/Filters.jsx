f2e21ac0bf2bca934428e5505f68a37bbfa085f5
Andrew Cook <andrewjcook93@gmail.com>
2025-05-13 12:43:19 -0500

    rm cash only filter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -30,17 +30,19 @@ const Filters = ({ list, selectedCategory, onFilterChange }) => {
 
   const activeBadges = badges
     .filter((badge) => {
+      if (badge.name === "cash only") {
+        return false; // Exclude 'cash only' completely
+      }
+
       // Special handling for 'paid parking' => display 'free parking'
       if (badge.name === "paid parking") {
         return hasPaidParking && hasFreeParkingOption;
       }
 
-      // Badge must exist on at least one place
       const appearsSomewhere = filteredList.some((place) =>
         place.badges?.includes(badge.name)
       );
 
-      // If badge appears on all places, don't include it
       const appearsEverywhere =
         appearsSomewhere &&
         filteredList.every((place) => place.badges?.includes(badge.name));
@@ -80,88 +82,3 @@ const Filters = ({ list, selectedCategory, onFilterChange }) => {
 };
 
 export default Filters;
-
-// import { useState, useEffect } from "react";
-// import badges from "../data/badges.js"; // Array of { name, img }
-
-// const Filters = ({ list, selectedCategory, onFilterChange }) => {
-//   const filteredList = list[selectedCategory] || [];
-
-//   const [selectedFilters, setSelectedFilters] = useState({});
-
-//   useEffect(() => {
-//     onFilterChange(selectedFilters);
-//   }, [selectedFilters, onFilterChange]);
-
-//   useEffect(() => {
-//     console.log("List updated:", filteredList);
-//   }, [selectedCategory, filteredList]);
-
-//   const handleFilterChange = (badgeName) => {
-//     setSelectedFilters((prev) => ({
-//       ...prev,
-//       [badgeName]: !prev[badgeName],
-//     }));
-//   };
-
-//   const hasPaidParking = filteredList.some((place) =>
-//     place.badges?.includes("paid parking")
-//   );
-//   const hasFreeParkingOption = filteredList.some(
-//     (place) => !place.badges?.includes("paid parking")
-//   );
-
-//   const activeBadges = badges
-//     .filter((badge) => {
-//       if (badge.name === "cash only") {
-//         return false; // Exclude 'cash only' completely
-//       }
-
-//       // Special handling for 'paid parking' => display 'free parking'
-//       if (badge.name === "paid parking") {
-//         return hasPaidParking && hasFreeParkingOption;
-//       }
-
-//       const appearsSomewhere = filteredList.some((place) =>
-//         place.badges?.includes(badge.name)
-//       );
-
-//       const appearsEverywhere =
-//         appearsSomewhere &&
-//         filteredList.every((place) => place.badges?.includes(badge.name));
-
-//       return appearsSomewhere && !appearsEverywhere;
-//     })
-//     .map((badge) => {
-//       if (badge.name === "paid parking") {
-//         return {
-//           name: "free parking",
-//           img: badge.img,
-//         };
-//       }
-//       return badge;
-//     });
-
-//   if (activeBadges.length === 0) {
-//     return null;
-//   }
-
-//   return (
-//     <div className="filters">
-//       <h3>Filters</h3>
-//       {activeBadges.map((badge) => (
-//         <div className="filter" key={badge.name}>
-//           <input
-//             type="checkbox"
-//             checked={selectedFilters[badge.name] || false}
-//             onChange={() => handleFilterChange(badge.name)}
-//           />
-//           {/* <img src={badge.img} alt={badge.name} /> */}
-//           <span>{badge.name}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Filters;
